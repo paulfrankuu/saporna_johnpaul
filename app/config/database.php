8c33f3d4f04a76b7366aad0b059e8788dc3af94b
paullamaw@gmail.com <paullamaw@gmail.com>
2025-09-12 21:31:33 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'crudui',
+    'username'	=> 'sql12798362',
+    'password'	=> 'k3udM8NqTC',
+    'database'	=> 'sql12798362',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
